rqk001/nuttx

move readv/writev to the kernel
currently, nuttx implements readv/writev on the top of read/write.
while it might work for the simplest cases, it's broken by design.
for example, it's impossible to make it work correctly for files
which need to preserve data boundaries without allocating a single
contiguous buffer. (udp socket, some character devices, etc)

this change is a start of the migration to a better design.
that is, implement read/write on the top of readv/writev.

to avoid a single huge change, following things will NOT be done in
this commit:

* fix actual bugs caused by the original readv-based-on-read design.
  (cf. https://github.com/apache/nuttx/pull/12674)

* adapt filesystems/drivers to actually benefit from the new interface.
  (except a few trivial examples)

* eventually retire the old interface.

* retire read/write syscalls. implement them in libc instead.

* pread/pwrite/preadv/pwritev (except the introduction of struct uio,
  which is a preparation to back these variations with the new
  interface.)

drivers/loop/loop.c
@@ -30,6 +30,7 @@
 #include <errno.h>
 
 #include <nuttx/fs/fs.h>
+#include <nuttx/fs/uio.h>
 #include <nuttx/fs/loop.h>
 
 #ifdef CONFIG_DEV_LOOP
@@ -38,10 +39,10 @@
  * Private Function Prototypes
  ****************************************************************************/
 
-static ssize_t loop_read(FAR struct file *filep, FAR char *buffer,
-                 size_t buflen);
-static ssize_t loop_write(FAR struct file *filep, FAR const char *buffer,
-                 size_t buflen);
+static ssize_t loop_readv(FAR struct file *filep,
+                          FAR const struct uio *uio);
+static ssize_t loop_writev(FAR struct file *filep,
+                           FAR const struct uio *uio);
 static int     loop_ioctl(FAR struct file *filep, int cmd,
                  unsigned long arg);
 
@@ -53,10 +54,15 @@ static const struct file_operations g_loop_fops =
 {
   NULL,          /* open */
   NULL,          /* close */
-  loop_read,     /* read */
-  loop_write,    /* write */
+  NULL,          /* read */
+  NULL,          /* write */
   NULL,          /* seek */
   loop_ioctl,    /* ioctl */
+  NULL,          /* mmap */
+  NULL,          /* truncate */
+  NULL,          /* poll */
+  loop_readv,    /* readv */
+  loop_writev    /* writev */
 };
 
 /****************************************************************************
@@ -67,8 +73,8 @@ static const struct file_operations g_loop_fops =
  * Name: loop_read
  ****************************************************************************/
 
-static ssize_t loop_read(FAR struct file *filep, FAR char *buffer,
-                         size_t len)
+static ssize_t loop_readv(FAR struct file *filep,
+                          FAR const struct uio *uio)
 {
   return 0; /* Return EOF */
 }
@@ -77,10 +83,10 @@ static ssize_t loop_read(FAR struct file *filep, FAR char *buffer,
  * Name: loop_write
  ****************************************************************************/
 
-static ssize_t loop_write(FAR struct file *filep, FAR const char *buffer,
-                          size_t len)
+static ssize_t loop_writev(FAR struct file *filep,
+                           FAR const struct uio *uio)
 {
-  return len; /* Say that everything was written */
+  return iovec_total_len(uio); /* Say that everything was written */
 }
 
 /****************************************************************************

drivers/misc/dev_null.c
@@ -32,16 +32,17 @@
 
 #include <nuttx/semaphore.h>
 #include <nuttx/fs/fs.h>
+#include <nuttx/fs/uio.h>
 #include <nuttx/drivers/drivers.h>
 
 /****************************************************************************
  * Private Function Prototypes
  ****************************************************************************/
 
-static ssize_t devnull_read(FAR struct file *filep, FAR char *buffer,
-                            size_t buflen);
-static ssize_t devnull_write(FAR struct file *filep, FAR const char *buffer,
-                             size_t buflen);
+static ssize_t devnull_readv(FAR struct file *filep,
+                             FAR const struct uio *uio);
+static ssize_t devnull_writev(FAR struct file *filep,
+                              FAR const struct uio *uio);
 static int     devnull_poll(FAR struct file *filep, FAR struct pollfd *fds,
                             bool setup);
 
@@ -51,15 +52,17 @@ static int     devnull_poll(FAR struct file *filep, FAR struct pollfd *fds,
 
 static const struct file_operations g_devnull_fops =
 {
-  NULL,          /* open */
-  NULL,          /* close */
-  devnull_read,  /* read */
-  devnull_write, /* write */
-  NULL,          /* seek */
-  NULL,          /* ioctl */
-  NULL,          /* mmap */
-  NULL,          /* truncate */
-  devnull_poll   /* poll */
+  NULL,           /* open */
+  NULL,           /* close */
+  NULL,           /* read */
+  NULL,           /* writev */
+  NULL,           /* seek */
+  NULL,           /* ioctl */
+  NULL,           /* mmap */
+  NULL,           /* truncate */
+  devnull_poll,   /* poll */
+  devnull_readv,  /* readv */
+  devnull_writev  /* writev */
 };
 
 /****************************************************************************
@@ -70,12 +73,11 @@ static const struct file_operations g_devnull_fops =
  * Name: devnull_read
  ****************************************************************************/
 
-static ssize_t devnull_read(FAR struct file *filep, FAR char *buffer,
-                            size_t len)
+static ssize_t devnull_readv(FAR struct file *filep,
+                             FAR const struct uio *uio)
 {
   UNUSED(filep);
-  UNUSED(buffer);
-  UNUSED(len);
+  UNUSED(uio);
 
   return 0; /* Return EOF */
 }
@@ -84,13 +86,12 @@ static ssize_t devnull_read(FAR struct file *filep, FAR char *buffer,
  * Name: devnull_write
  ****************************************************************************/
 
-static ssize_t devnull_write(FAR struct file *filep, FAR const char *buffer,
-                             size_t len)
+static ssize_t devnull_writev(FAR struct file *filep,
+                              FAR const struct uio *uio)
 {
   UNUSED(filep);
-  UNUSED(buffer);
 
-  return len; /* Say that everything was written */
+  return iovec_total_len(uio); /* Say that everything was written */
 }
 
 /****************************************************************************

drivers/misc/dev_zero.c
@@ -32,16 +32,17 @@
 
 #include <nuttx/semaphore.h>
 #include <nuttx/fs/fs.h>
+#include <nuttx/fs/uio.h>
 #include <nuttx/drivers/drivers.h>
 
 /****************************************************************************
  * Private Function Prototypes
  ****************************************************************************/
 
-static ssize_t devzero_read(FAR struct file *filep, FAR char *buffer,
-                            size_t buflen);
-static ssize_t devzero_write(FAR struct file *filep, FAR const char *buffer,
-                             size_t buflen);
+static ssize_t devzero_readv(FAR struct file *filep,
+                             FAR const struct uio *uio);
+static ssize_t devzero_writev(FAR struct file *filep,
+                              FAR const struct uio *uio);
 static int     devzero_poll(FAR struct file *filep, FAR struct pollfd *fds,
                             bool setup);
 
@@ -51,15 +52,17 @@ static int     devzero_poll(FAR struct file *filep, FAR struct pollfd *fds,
 
 static const struct file_operations g_devzero_fops =
 {
-  NULL,          /* open */
-  NULL,          /* close */
-  devzero_read,  /* read */
-  devzero_write, /* write */
-  NULL,          /* seek */
-  NULL,          /* ioctl */
-  NULL,          /* mmap */
-  NULL,          /* truncate */
-  devzero_poll   /* poll */
+  NULL,           /* open */
+  NULL,           /* close */
+  NULL,           /* read */
+  NULL,           /* write */
+  NULL,           /* seek */
+  NULL,           /* ioctl */
+  NULL,           /* mmap */
+  NULL,           /* truncate */
+  devzero_poll,   /* poll */
+  devzero_readv,  /* readv */
+  devzero_writev  /* writev */
 };
 
 /****************************************************************************
@@ -70,26 +73,39 @@ static const struct file_operations g_devzero_fops =
  * Name: devzero_read
  ****************************************************************************/
 
-static ssize_t devzero_read(FAR struct file *filep, FAR char *buffer,
-                            size_t len)
+static ssize_t devzero_readv(FAR struct file *filep,
+                             FAR const struct uio *uio)
 {
+  ssize_t total =  iovec_total_len(uio);
+  int i;
+
   UNUSED(filep);
 
-  memset(buffer, 0, len);
-  return len;
+  if (total < 0)
+    {
+      return total;
+    }
+
+  FAR const struct iovec *iov = uio->uio_iov;
+  int iovcnt = uio->uio_iovcnt;
+  for (i = 0; i < iovcnt; i++)
+    {
+      memset(iov[i].iov_base, 0, iov[i].iov_len);
+    }
+
+  return total;
 }
 
 /****************************************************************************
  * Name: devzero_write
  ****************************************************************************/
 
-static ssize_t devzero_write(FAR struct file *filep, FAR const char *buffer,
-                             size_t len)
+static ssize_t devzero_writev(FAR struct file *filep,
+                              FAR const struct uio *uio)
 {
   UNUSED(filep);
-  UNUSED(buffer);
 
-  return len;
+  return iovec_total_len(uio);
 }
 
 /****************************************************************************

guohao15/vela_nuttx

drivers/loop/loop.c
@@ -38,10 +38,10 @@
  * Private Function Prototypes
  ****************************************************************************/
 
-static ssize_t loop_read(FAR struct file *filep, FAR char *buffer,
-                 size_t buflen);
-static ssize_t loop_write(FAR struct file *filep, FAR const char *buffer,
-                 size_t buflen);
+static ssize_t loop_readv(FAR struct file *filep,
+                          FAR const struct uio *uio);
+static ssize_t loop_writev(FAR struct file *filep,
+                           FAR const struct uio *uio);
 static int     loop_ioctl(FAR struct file *filep, int cmd,
                  unsigned long arg);
 
@@ -53,10 +53,15 @@ static const struct file_operations g_loop_fops =
 {
   NULL,          /* open */
   NULL,          /* close */
-  loop_read,     /* read */
-  loop_write,    /* write */
+  NULL,          /* read */
+  NULL,          /* write */
   NULL,          /* seek */
   loop_ioctl,    /* ioctl */
+  NULL,          /* mmap */
+  NULL,          /* truncate */
+  NULL,          /* poll */
+  loop_readv,    /* readv */
+  loop_writev    /* writev */
 };
 
 /****************************************************************************
@@ -67,8 +72,8 @@ static const struct file_operations g_loop_fops =
  * Name: loop_read
  ****************************************************************************/
 
-static ssize_t loop_read(FAR struct file *filep, FAR char *buffer,
-                         size_t len)
+static ssize_t loop_readv(FAR struct file *filep,
+                          FAR const struct uio *uio)
 {
   return 0; /* Return EOF */
 }
@@ -77,10 +82,10 @@ static ssize_t loop_read(FAR struct file *filep, FAR char *buffer,
  * Name: loop_write
  ****************************************************************************/
 
-static ssize_t loop_write(FAR struct file *filep, FAR const char *buffer,
-                          size_t len)
+static ssize_t loop_writev(FAR struct file *filep,
+                           FAR const struct uio *uio)
 {
-  return len; /* Say that everything was written */
+  return uio_total_len(uio); /* Say that everything was written */
 }
 
 /****************************************************************************

drivers/misc/dev_null.c
@@ -38,10 +38,10 @@
  * Private Function Prototypes
  ****************************************************************************/
 
-static ssize_t devnull_read(FAR struct file *filep, FAR char *buffer,
-                            size_t buflen);
-static ssize_t devnull_write(FAR struct file *filep, FAR const char *buffer,
-                             size_t buflen);
+static ssize_t devnull_readv(FAR struct file *filep,
+                             FAR const struct uio *uio);
+static ssize_t devnull_writev(FAR struct file *filep,
+                              FAR const struct uio *uio);
 static int     devnull_poll(FAR struct file *filep, FAR struct pollfd *fds,
                             bool setup);
 
@@ -51,15 +51,17 @@ static int     devnull_poll(FAR struct file *filep, FAR struct pollfd *fds,
 
 static const struct file_operations g_devnull_fops =
 {
-  NULL,          /* open */
-  NULL,          /* close */
-  devnull_read,  /* read */
-  devnull_write, /* write */
-  NULL,          /* seek */
-  NULL,          /* ioctl */
-  NULL,          /* mmap */
-  NULL,          /* truncate */
-  devnull_poll   /* poll */
+  NULL,           /* open */
+  NULL,           /* close */
+  NULL,           /* read */
+  NULL,           /* writev */
+  NULL,           /* seek */
+  NULL,           /* ioctl */
+  NULL,           /* mmap */
+  NULL,           /* truncate */
+  devnull_poll,   /* poll */
+  devnull_readv,  /* readv */
+  devnull_writev  /* writev */
 };
 
 /****************************************************************************
@@ -70,12 +72,11 @@ static const struct file_operations g_devnull_fops =
  * Name: devnull_read
  ****************************************************************************/
 
-static ssize_t devnull_read(FAR struct file *filep, FAR char *buffer,
-                            size_t len)
+static ssize_t devnull_readv(FAR struct file *filep,
+                             FAR const struct uio *uio)
 {
   UNUSED(filep);
-  UNUSED(buffer);
-  UNUSED(len);
+  UNUSED(uio);
 
   return 0; /* Return EOF */
 }
@@ -84,13 +85,12 @@ static ssize_t devnull_read(FAR struct file *filep, FAR char *buffer,
  * Name: devnull_write
  ****************************************************************************/
 
-static ssize_t devnull_write(FAR struct file *filep, FAR const char *buffer,
-                             size_t len)
+static ssize_t devnull_writev(FAR struct file *filep,
+                              FAR const struct uio *uio)
 {
   UNUSED(filep);
-  UNUSED(buffer);
 
-  return len; /* Say that everything was written */
+  return uio_total_len(uio); /* Say that everything was written */
 }
 
 /****************************************************************************

drivers/misc/dev_zero.c
@@ -38,10 +38,10 @@
  * Private Function Prototypes
  ****************************************************************************/
 
-static ssize_t devzero_read(FAR struct file *filep, FAR char *buffer,
-                            size_t buflen);
-static ssize_t devzero_write(FAR struct file *filep, FAR const char *buffer,
-                             size_t buflen);
+static ssize_t devzero_readv(FAR struct file *filep,
+                             FAR const struct uio *uio);
+static ssize_t devzero_writev(FAR struct file *filep,
+                              FAR const struct uio *uio);
 static int     devzero_poll(FAR struct file *filep, FAR struct pollfd *fds,
                             bool setup);
 
@@ -51,15 +51,17 @@ static int     devzero_poll(FAR struct file *filep, FAR struct pollfd *fds,
 
 static const struct file_operations g_devzero_fops =
 {
-  NULL,          /* open */
-  NULL,          /* close */
-  devzero_read,  /* read */
-  devzero_write, /* write */
-  NULL,          /* seek */
-  NULL,          /* ioctl */
-  NULL,          /* mmap */
-  NULL,          /* truncate */
-  devzero_poll   /* poll */
+  NULL,           /* open */
+  NULL,           /* close */
+  NULL,           /* read */
+  NULL,           /* write */
+  NULL,           /* seek */
+  NULL,           /* ioctl */
+  NULL,           /* mmap */
+  NULL,           /* truncate */
+  devzero_poll,   /* poll */
+  devzero_readv,  /* readv */
+  devzero_writev  /* writev */
 };
 
 /****************************************************************************
@@ -70,26 +72,39 @@ static const struct file_operations g_devzero_fops =
  * Name: devzero_read
  ****************************************************************************/
 
-static ssize_t devzero_read(FAR struct file *filep, FAR char *buffer,
-                            size_t len)
+static ssize_t devzero_readv(FAR struct file *filep,
+                             FAR const struct uio *uio)
 {
+  ssize_t total =  uio_total_len(uio);
+  FAR const struct iovec *iov = uio->uio_iov;
+  int iovcnt = uio->uio_iovcnt;
+  int i;
+
   UNUSED(filep);
 
-  memset(buffer, 0, len);
-  return len;
+  if (total < 0)
+    {
+      return total;
+    }
+
+  for (i = 0; i < iovcnt; i++)
+    {
+      memset(iov[i].iov_base, 0, iov[i].iov_len);
+    }
+
+  return total;
 }
 
 /****************************************************************************
  * Name: devzero_write
  ****************************************************************************/
 
-static ssize_t devzero_write(FAR struct file *filep, FAR const char *buffer,
-                             size_t len)
+static ssize_t devzero_writev(FAR struct file *filep,
+                              FAR const struct uio *uio)
 {
   UNUSED(filep);
-  UNUSED(buffer);
 
-  return len;
+  return uio_total_len(uio);
 }
 
 /****************************************************************************